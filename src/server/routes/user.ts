b23bb2bade0307d8427140e7bd021f65ac2c8aab
Emmanuel-Begati <begati16@gmail.com>
2025-07-01 18:07:29 +0200

    starting over

--- src/server/routes/user.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-const router = Router();
-
-router.get('/profile', (req, res) => {
-  res.json({ success: true, data: { message: 'User profile endpoint' } });
-});
-
-export default router;
--- tsnode.esm.config.js
+++
@@ -1,7 +0,0 @@
-import { register } from 'ts-node';
-
-register({
-  esm: true,
-  project: './tsconfig.server.json',
-  experimentalSpecifierResolution: 'node'
-});
